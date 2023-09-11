65cb763be6db9258ddfe64e0092eb4c6b87b35ff
Edison-Mondal <mondaledison@gmail.com>
2023-09-11 11:47:45 +0600

    ok

--- routes/api.js
+++ routes/api.js
@@ -69,7 +69,11 @@ router.post('/users/:_id/exercises', (req, res) => {
   const lastLog = users[index]['log'][ users[index]['log'].length - 1]
   //res.json({ username: users[index]['username'], description: lastLog['description'], duration: lastLog['duration'], date: lastLog['date'], _id: users[index]['_id'] });
   
-  const exercise = { username: users[index]['username'], description: lastLog['description'], duration: lastLog['duration'], date: lastLog['date'], _id: users[index]['_id'] }
+  const exercise = { 
+    username: users[index]['username'],
+    _id: users[index]['_id'],
+    log: users[index]['log'],
+   }
   /*
   exercise['_id'] = users[index]['_id'],
   exercise['username'] = users[index]['username'], 
