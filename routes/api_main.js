0d627e22015e959242b0a651f82279eb34db1f03
Edison-Mondal <mondaledison@gmail.com>
2023-09-12 10:22:24 +0600

    re

---
+++ routes/api_main.js
@@ -0,0 +1,220 @@
+// Import necessary modules
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+
+
+// Sample data storage (replace with a database in production)
+const users = [];
+//const exercise = {};
+
+// POST /api/users to create a new user
+router.post('/users', (req, res) => {
+  const { username } = req.body;
+  const newUserId = uuid.v4(); // Generates a random UUID in hexadecimal format
+  const newUser = { username, _id: newUserId };
+  users.push(newUser);
+  res.json(newUser);
+});
+
+
+
+// GET /api/users to get a list of all users
+router.get('/users', (req, res) => {
+
+  res.json(users);
+  
+});
+
+
+
+// POST /api/users/:_id/exercises to add a new exercise for a user
+router.post('/users/:_id/exercises', (req, res) => {
+  const { _id } = req.params;
+  const { description, duration, date } = req.body;
+
+  // Create an exercise log entry
+  const logEntry = { description, duration: parseInt(duration), date: date || new Date().toDateString() };
+
+
+  let found = false;
+  let index = 0
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]['_id'] === _id ) {
+        found = true;
+        index = i
+        
+        break;
+    }
+  }
+  
+  if(!found){
+    res.status(404).json({ message: 'User not found' });
+    return;
+  }
+  
+  /*
+  if(found){
+    // count
+    if(!users[index]['count']){
+        users[index]['count'] = 0
+    }
+    // log array
+    if(!users[index]['log']){
+        users[index]['log'] = []
+    }
+
+    users[index]['log'].push(logEntry);
+    users[index]['count']++
+
+    const lastLog = users[index]['log'][ users[index]['log'].length - 1]
+
+    const exercise = { username: users[index]['username'], description: lastLog['description'], duration: lastLog['duration'], date: lastLog['date'], _id: users[index]['_id'] }
+
+    return res.json( exercise )
+  } else {
+    res.status(404).json({ message: 'User not found' });
+    return;
+  }
+  */
+
+ // count
+ if(!users[index]['count']){
+    users[index]['count'] = 0
+}
+// log array
+if(!users[index]['log']){
+    users[index]['log'] = []
+}
+
+users[index]['log'].push(logEntry);
+users[index]['count']++
+
+
+
+
+
+
+
+
+  
+  console.warn( users[index] )
+  
+  
+  const lastLog = users[index]['log'][ users[index]['log'].length - 1]
+  
+  
+  const exercise = { username: users[index]['username'], description: lastLog['description'], duration: lastLog['duration'], date: lastLog['date'], _id: users[index]['_id'] }
+  
+  
+
+  res.json( exercise )
+  
+  
+});
+
+
+// GET /api/users/:_id/logs to retrieve a user's exercise log
+router.get('/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  const { from, to, limit } = req.query;
+  /*
+  if (!exerciseLogs[_id]) {
+    res.status(404).json({ message: 'User not found' });
+    return;
+  }
+  */
+  let found = false;
+  let index = 0
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]['_id'] === _id ) {
+        found = true;
+        index = i
+        
+        break;
+    }
+  }
+
+  if(!found){
+    res.status(404).json({ message: 'User not found' });
+    return;
+  } 
+  else {
+
+    let log = users[index]['log']
+    /*
+    let queryArr = []
+
+        
+    for (let i = 0; i < log.length; i++){
+        const olddate = log[i]['date']
+        const oldYear = olddate.getFullYear()
+        const oldMonth = olddate.getMonth()
+        const oldDate = olddate.getDate()
+
+        const querydatefrom = new Date(from)
+        const queryYearfrom = querydatefrom.getFullYear()
+        const queryMonthfrom = querydatefrom.getMonth()
+        const queryDatafrom = querydatefrom.getDate()
+
+        const querydateto = new Date(from)
+        const queryYearto = querydateto.getFullYear()
+        const queryMonthto = querydateto.getMonth()
+        const queryDatato = querydateto.getDate()
+
+        if(from != undefined && to != undefined && (oldYear >= queryYearfrom && oldMonth >= queryMonthfrom && oldDate>= queryDatafrom ) && (oldYear <= queryYearto && oldMonth <= queryMonthto && oldDate < queryDatato) ){
+            queryArr.push(log[i])
+        }else if(from != undefined && (oldYear >= queryYearfrom && oldMonth >= queryMonthfrom && oldDate>= queryDatafrom ) ){ 
+            queryArr.push(log[i])
+        } else if(to != undefined && (oldYear <= queryYearto && oldMonth <= queryMonthto && oldDate < queryDatato) ) {
+            queryArr.push(log[i])
+        }
+
+        if(queryArr.length == parseInt(limit)){
+            break
+        }
+    }
+    */
+    
+    
+    
+    // Filter logs based on 'from' and 'to' dates
+    if (from || to) {
+        log = log.filter(entry => {
+        const entryDate = new Date(entry.date);
+        return (!from || entryDate >= new Date(from)) && (!to || entryDate <= new Date(to));
+        });
+    }
+
+    // Limit the number of logs returned
+    if (limit) {
+        log = log.slice(0, parseInt(limit));
+    }
+    
+    
+    
+    //console.warn(log)
+    //console.warn(queryArr)
+    
+    const resObj = {}
+    resObj['_id'] = users[index]['_id']
+    resObj['username'] = users[index]['username']
+    if(from != undefined && !isNaN(new Date(from))){
+        resObj['from'] = from
+    }
+    if(to != undefined && !isNaN(new Date(to))){
+        resObj['to'] = to
+    }
+    resObj['count'] = log.length
+    resObj['log'] = log
+    //resObj['count'] = queryArr.length
+    //resObj['log'] = queryArr
+
+    return res.json( resObj );
+ }
+  
+});
+
+module.exports = router;
