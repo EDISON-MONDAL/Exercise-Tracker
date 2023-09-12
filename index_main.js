2a9b9040c5e7f594879bac4b800c0129deaa4d39
Edison-Mondal <mondaledison@gmail.com>
2023-09-12 08:53:00 +0600

    mongo

---
+++ index_main.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const apiRouter = require('./routes/api');
+
+
+// Middleware to parse JSON and form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+app.use(cors())
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+
+
+// router
+app.use('/api', apiRouter);
+
+
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
